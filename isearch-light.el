e292f73689c5857784fdcd509d5ee73cf7bfef41
Thierry Volpiatto <thievol@posteo.net>
2021-01-09 14:01:06 +0100

    Update headers

--- isearch-light.el
+++ isearch-light.el
@@ -1,7 +1,19 @@
 ;;; isearch-light.el --- simple incremental search in current-buffer -*- lexical-binding: t -*- 
 
-;	$Id: isearch-light.el,v 1.7 2021/01/06 16:05:31 thierry Exp thierry $	
+;; Copyright (C) 2012 ~ 2020 Thierry Volpiatto <thierry.volpiatto@gmail.com>
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Code:
 
