c99dc44f604e30dc8c06f89edac37eba460d7351
Thierry Volpiatto <thievol@posteo.net>
2025-11-20 05:41:29 +0100

    Bump to 1.6

--- isl.el
+++ isl.el
@@ -3,7 +3,7 @@
 ;; Author:      Thierry Volpiatto <thievol@posteo.net>
 ;; Copyright (C) 2021~2025 Thierry Volpiatto <thievol@posteo.net>
 
-;; Version: 1.4
+;; Version: 1.6
 ;; URL: https://github.com/thierryvolpiatto/isearch-light
 
 ;; Compatibility: GNU Emacs 29.1+
