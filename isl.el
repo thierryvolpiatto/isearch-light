a998ee1c9a1b682f6fbc5eced4c8e22605b65eef
Thierry Volpiatto <thievol@posteo.net>
2026-04-14 08:20:54 +0200

    Add Keywords header

--- isl.el
+++ isl.el
@@ -8,6 +8,7 @@
 
 ;; Compatibility: GNU Emacs 29.1+
 ;; Package-Requires: ((emacs "29.1"))
+;; Keywords: matching, searching
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
